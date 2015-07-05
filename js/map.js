0cf7c0103f89b3450e0d88c69792a7a243fbcc28
Austin Meyers <meyersaustin22@gmail.com>
2015-07-05 15:06:28 -0700

    Removed icon awesome library

--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@ var drawMap = function() {
 
   // Create map and set viewd
 	var map = L.map('map');
-	map.setView([34,-100],3);
+	map.setView([34,-100],2);
   // Create an tile layer variable using the appropriate url
 	var layer = L.tileLayer('https://{s}.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYXJtMjA2IiwiYSI6IjJlMjQ1NWRlOTIyY2Q0MmMwMTE5ZWFmNTgyOTI5NjhkIn0.Avm6VjzhgLYy-YOsv61aCQ');
   // Add the layer to your map
@@ -26,11 +26,10 @@ var getData = function(map) {
 
 // Do something creative with the data here!  
 var customBuild = function(data, map) {
-	L.AwesomeMarkers.Icon.prototype.options.prefix = 'fa';
 	console.log(data);
 	data.map(function(d) {
 		if (d["Victim's Gender"] == "Male") {
-			var circle = new L.circle([d.lat, d.lng], 100, {color:'#34495E', opacity:.7}).addTo(map);
+			var circle = new L.circle([d.lat, d.lng], 100, {color:'#4B77BE', opacity:.7}).addTo(map);
 		} else if (d["Victim's Gender"] == "Female") {
 			var circle = new L.circle([d.lat, d.lng], 100, {color:'#D2527F', opacity:.7}).addTo(map);
 		}
