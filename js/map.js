07f5aea36b86450ab7fabb0e5d399df12cffb870
Austin Meyers <meyersaustin22@gmail.com>
2015-07-04 20:03:00 -0700

    Working with JSON

--- js/map.js
+++ js/map.js
@@ -15,7 +15,7 @@ var drawMap = function() {
 // Function for getting data
 var getData = function(map) {
 	$.ajax({
-	    url:'../data/response.json',
+	    url:'./data/response.json',
 	        type: "get",
 		    success: function(dat){
 		    	customBuild(dat, map);
